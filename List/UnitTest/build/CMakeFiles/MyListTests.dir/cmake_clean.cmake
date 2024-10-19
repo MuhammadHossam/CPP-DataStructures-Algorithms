d79edd38f1048da7ef7cd1004cb9947fe2db2e21
Muhammad <mohamedhossameldin78@gmail.com>
2024-10-19 15:10:05 +0300

    - Solve the remove method bug. - Implement unit test suite for the list.

---
+++ List/UnitTest/build/CMakeFiles/MyListTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyListTests.dir/MyTests.cpp.o"
+  "CMakeFiles/MyListTests.dir/MyTests.cpp.o.d"
+  "MyListTests"
+  "MyListTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyListTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ List/UnitTest/build/CMakeFiles/MyListTests.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyListTests.
+# This may be replaced when dependencies are built.
